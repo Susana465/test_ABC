523709a7358433123ebed42a651c1d15d100a7c6
Susana <s1350728@ed.ac.uk>
2024-04-11 12:46:41 +0100

    plot script takes on a file from command line and saves it in same folder as the .gdat it has used

--- plot_output.py
+++ plot_output.py
@@ -1,37 +1,46 @@
+# This script is to be run in command line as $ python plot_output.py
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Insert file you want to plot:
-target_file = "2024-04-10_16-29-35_out.gdat"
+def plot_target_file(target_file):
+    # Function to search for the target file within directories containing "run_"
+    def find_target_file(root_dir, target_filename):
+        for root, dirs, files in os.walk(root_dir):
+            for directory in dirs:
+                if "run_" in directory:
+                    target_filepath = os.path.join(root, directory, target_filename)
+                    if os.path.isfile(target_filepath):
+                        return target_filepath
+        return None
 
-# Function to search for the target file within directories containing "run_"
-def find_target_file(root_dir, target_filename):
-    for root, dirs, files in os.walk(root_dir):
-        for directory in dirs:
-            if "run_" in directory:
-                target_filepath = os.path.join(root, directory, target_filename)
-                if os.path.isfile(target_filepath):
-                    return target_filepath
-    return None
+    # Search for the target file within the current directory "."
+    target_filepath = find_target_file(".", target_file)
 
-# Search for the target file within the current directory "."
-target_filepath = find_target_file(".", target_file)
+    if target_filepath is not None:
+        print("Using file:", target_filepath)
 
-if target_filepath is not None:
-    print("Using file:", target_filepath)
+        data = np.loadtxt(fname=target_filepath)
+        data_new = np.delete(data, 0, 1)
 
-    data = np.loadtxt(fname=target_filepath)
-    data_new = np.delete(data, 0, 1)
+        plt.xlabel("Iterations")
+        plt.ylabel("Molecule count")
+        plt.title("Molecule counts through time")
 
-    plt.xlabel("Iterations")
-    plt.ylabel("Molecule count")
-    plt.title("A+B->C")
+        plt.plot(data_new)
 
-    plt.plot(data_new)
+        # Save the .png file using the name of the .gdat file
+        target_directory = os.path.dirname(target_filepath)
+        # extract the filename without extension from the target_filepath
+        target_filename_no_ext = os.path.splitext(os.path.basename(target_filepath))[0]
+        # use target_filename_no_ext to save the .png file using the same filename as the .gdat file but with a .png extension
+        plt.savefig(os.path.join(target_directory, f"{target_filename_no_ext}.png"), dpi=500)
+        plt.show()
+        # Notify the user about the saved .png file
+        print(f"Your plot has now been saved in {os.path.join(target_directory, f'{target_filename_no_ext}.png')}")
+    else:
+        print(f"File '{target_file}' not found in any directory containing 'run_'")
 
-    plt.savefig("high_res.png", dpi=500)
-    plt.show()
-else:
-    print(f"File '{target_file}' not found in any directory containing 'run_'")
-
+if __name__ == "__main__":
+    target_file = input("Which .gdat file would you like to plot? ")
+    plot_target_file(target_file)
